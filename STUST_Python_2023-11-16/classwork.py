b0cb47ae0e1e143e1eb6830e2cf31025c41e8c0e
劉定宗 <91250803+LUDEANDOWN@users.noreply.github.com>
2023-11-16 11:58:36 +0800

    Update classwork.py

--- STUST_Python_2023-11-16/classwork.py
+++ STUST_Python_2023-11-16/classwork.py
@@ -1,7 +1,7 @@
 import math
 
 
-class MyShape:
+class MyShape: 
     def __init__(self,square_side,length,width,radius):
         self.square_side=square_side
         self.length=length
@@ -9,16 +9,16 @@ class MyShape:
         self.radius=radius
 
     def getSquareArea(self):
-        print("正方形面積=" + str(self.square_side*self.square_side))
+        print("正方形面積=" + str(self.square_side*self.square_side)) #打印正方形面積函式
 
     def getRectangleArea(self):
-        print("長方形面積="+str(self.length*self.width))
+        print("長方形面積="+str(self.length*self.width))#打印長方形面積函式
 
     def getTriangleArea(self):
-        print("三角形面積="+ str((self.length*self.width)/2))
+        print("三角形面積="+ str((self.length*self.width)/2))#打印三角形面積函式
 
     def getCircleArea(self):
-        print("圓形面積="+str(self.radius*self.radius*math.pi))
+        print("圓形面積="+str(self.radius*self.radius*math.pi))#打印圓形面積函式
 
 
 SP=MyShape(2,4,6,8)
